b14c98e017c752c5b73ab0f1187c8f3afe234f1f
Simone Lopes <sisilopes20@gmail.com>
2023-05-11 22:59:45 +0100

    add font configuration

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,8 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Roboto:wght@400;700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
+  }
+
+  html {
+    font-size: 62.5%; //allows to use rem like a decimal 2.4 = 24px ; 2.2rem = 22px ...
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  h1 {
+    font-size: 2.4rem;
+    font-family: 'Montserrat', sans-serif;
   }
 `;
