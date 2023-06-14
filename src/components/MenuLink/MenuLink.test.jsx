069bbc37a63bd5cfa9878b37645fee34e598daf5
Simone Lopes <sisilopes20@gmail.com>
2023-06-14 18:21:34 +0100

    add MenuLink component

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react';
+import { MenuLink } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">Children</MenuLink>);
+    expect(screen.getByRole('link', { name: 'Children' })).toBeInTheDocument();
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={false}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toMatchSnapshot();
+  });
+});
