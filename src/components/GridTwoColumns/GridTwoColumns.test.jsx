270a26e7577f10b8ff0784afa8c0283e01578063
Simone Lopes <sisilopes20@gmail.com>
2023-07-04 23:39:47 +0100

    create GridTwoColumns component including storybook and test

---
+++ src/components/GridTwoColumns/GridTwoColumns.test.jsx
@@ -0,0 +1,11 @@
+import { GridTwoColumns } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+import mock from './mock';
+
+describe('<GridTwoColumns />', () => {
+  it('should render two column grid', () => {
+    const { container } = renderTheme(<GridTwoColumns {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/GridTwoColumns/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid Two Columns',
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi exercitationem doloribus perferendis odio unde veniam consectetur voluptate facilis nulla culpa dolore voluptas blanditiis praesentium, earum saepe, ad molestiae ut ipsa.`,
+  srcImg: 'assets/images/javascript.svg',
+};
